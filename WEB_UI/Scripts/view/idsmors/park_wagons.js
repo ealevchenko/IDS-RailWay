2f632c0f3a08ed4f002b04a9d80f90a248a6de39
eduard.levchenko@arcelormittal.com <eduard.levchenko@arcelormittal.com>
2020-01-23 23:19:51 +0200

    panel edit

--- WEB_UI/Scripts/view/idsmors/park_wagons.js
+++ WEB_UI/Scripts/view/idsmors/park_wagons.js
@@ -18,40 +18,46 @@
                 }
             });
         },
-        Open: function (trk_num, side) {
-            if (trk_num && side) {
-                // пистолеты
-                if (trk_num > 0 && trk_num < 10) {
-                    confirm_rfid_card.obj.dialog("option", "title", 'RFID-карта (Колонка №' + trk_num + ', сторона :' + (side == 0 ? 'левая' : 'правая') + ')');
-                }
-                // Наливной стояк
-                if (trk_num >= 10 && trk_num <= 12) {
-                    var num = trk_num - 9;
-                    confirm_rfid_card.obj.dialog("option", "title", 'RFID-карта (Наливной стояк №' + num + ')');
-                }
-                confirm_rfid_card.obj.dialog("open");
-                var card = cards.getCardOfNumSide(trk_num, side);
-                if (card) {
-                    $('#id').val(card.Id);
-                    $('#Number').val(card.Number);
-                    $('#DriverName').val(card.DriverName);
-                    $('#AutoNumber').val(card.AutoNumber);
-                    $('#Debitor').val(card.Debitor);
-                    $('#Sn1').val(card.Sn1);
-                    $('#Sn2').val(card.Sn2);
-                    $('#AutoModel').val(card.AutoModel);
-                    $('#Street').val(card.Street);
-                    $('#House').val(card.Name);
-                    $('#CreateDate').val(card.CreateDate);
-                    $('#CreateTime').val(card.CreateTime);
-                    $('#UpdateDate').val(card.UpdateDate);
-                    $('#UpdateTime').val(card.UpdateTime);
-                    $('#Owner').val(card.Owner);
-                    $('#Active').prop('checked', card.Active);
-                }
-            }
+        Open: function (id) {
+            confirm_rfid_card.obj.dialog("open");
+            //if (id) {
+            //    // пистолеты
+            //    if (trk_num > 0 && trk_num < 10) {
+            //        confirm_rfid_card.obj.dialog("option", "title", 'RFID-карта (Колонка №' + trk_num + ', сторона :' + (side == 0 ? 'левая' : 'правая') + ')');
+            //    }
+            //    // Наливной стояк
+            //    if (trk_num >= 10 && trk_num <= 12) {
+            //        var num = trk_num - 9;
+            //        confirm_rfid_card.obj.dialog("option", "title", 'RFID-карта (Наливной стояк №' + num + ')');
+            //    }
+
+            //    var card = cards.getCardOfNumSide(trk_num, side);
+            //    if (card) {
+            //        $('#id').val(card.Id);
+            //        $('#Number').val(card.Number);
+            //        $('#DriverName').val(card.DriverName);
+            //        $('#AutoNumber').val(card.AutoNumber);
+            //        $('#Debitor').val(card.Debitor);
+            //        $('#Sn1').val(card.Sn1);
+            //        $('#Sn2').val(card.Sn2);
+            //        $('#AutoModel').val(card.AutoModel);
+            //        $('#Street').val(card.Street);
+            //        $('#House').val(card.Name);
+            //        $('#CreateDate').val(card.CreateDate);
+            //        $('#CreateTime').val(card.CreateTime);
+            //        $('#UpdateDate').val(card.UpdateDate);
+            //        $('#UpdateTime').val(card.UpdateTime);
+            //        $('#Owner').val(card.Owner);
+            //        $('#Active').prop('checked', card.Active);
+            //    }
+            //}
         }
     };
-    confirm_rfid_card.init()
+
+    confirm_rfid_card.init();
+
+    $('button#add-park').on('click', function () {
+        confirm_rfid_card.Open();
+    });
 });
 
