5e23e9db0b2aff01c672f95b40bbc1ef8ae400a0
Levchenko <eduard.levchenko@arcelormittal.com>
2020-01-31 17:42:26 +0200

    start

---
+++ WEB_UI/Areas/IDSRWT/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB_UI.Areas.IDSRWT.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: IDSRWT/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Arrival()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEB_UI/Areas/IDSRWT/IDSRWTAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace WEB_UI.Areas.IDSRWT
+{
+    public class IDSRWTAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "IDSRWT";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "IDSRWT_default",
+                "IDSRWT/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WEB_UI.Areas.IDSRWT.Controllers" }
+            );
+        }
+    }
+}
