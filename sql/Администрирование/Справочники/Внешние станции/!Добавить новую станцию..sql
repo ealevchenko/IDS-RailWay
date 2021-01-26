63e46b2a03d9da72a4627cfb64620ac3d9f097f1
Levchenko <eduard.levchenko@arcelormittal.com>
2021-01-26 15:13:59 +0200

    sql

---
+++ sql/Администрирование/Справочники/Внешние станции/!Добавить новую станцию..sql
@@ -0,0 +1,24 @@
+USE [KRR-PA-CNT-Railway]
+GO
+
+--INSERT INTO [IDS].[Directory_ExternalStation]
+--           ([code]
+--           ,[station_name_ru]
+--           ,[station_name_en]
+--           ,[code_inlandrailway]
+--           ,[create]
+--           ,[create_user]
+--           ,[change]
+--           ,[change_user])
+--     VALUES
+--           (334103
+--           ,N'—“¿–Œ ŒÕ—“¿Õ“»ÕŒ¬ 1'
+--           ,N'STAROKONSTANTINOV 1'
+--           ,32
+--           ,GetDate()
+--           ,N'EUROPE\ealevchenko'
+--           ,null
+--           ,null)
+GO
+
+
