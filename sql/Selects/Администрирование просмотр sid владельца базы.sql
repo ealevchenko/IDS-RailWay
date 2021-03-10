57b19536005a7f7bc18dff09398862eb4ae0dc6f
eduard.levchenko@arcelormittal.com <eduard.levchenko@arcelormittal.com>
2021-03-10 17:44:58 +0200

    select

--- sql/Selects/Администрирование просмотр sid владельца базы.sql
+++ sql/Selects/Администрирование просмотр sid владельца базы.sql
@@ -5,17 +5,18 @@
 SELECT p.name, p.sid, p.type_desc FROM
 sys.databases AS d LEFT OUTER JOIN sys.server_principals AS p ON
 d.owner_sid = p.sid 
-WHERE d.name = N'loko_nw'
+WHERE d.name = N'KRR-PA-CNT-Railway'
 
 -- Смотрим кто владелец базы
 SELECT CAST(owner_sid as uniqueidentifier) AS Owner_SID   
 FROM sys.databases   
-WHERE name = 'loko_nw';  
+WHERE name = 'KRR-PA-CNT-Railway';  
 --WHERE name = 'dp9';  
 
 --SELECT SUSER_SID('sa');
 --SELECT SUSER_SID('HP_EDIK\edik');
 --SELECT SUSER_SID('dbo');
+SELECT SUSER_SID('DESKTOP-KHSSI53\user');
 
 --> Этой командой привязываем нового
---ALTER AUTHORIZATION ON DATABASE ::[loko_nw] TO [sa]
+--ALTER AUTHORIZATION ON DATABASE ::[KRR-PA-CNT-Railway] TO [DESKTOP-KHSSI53\user]
