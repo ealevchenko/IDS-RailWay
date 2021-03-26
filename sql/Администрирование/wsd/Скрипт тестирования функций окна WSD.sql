bb7fa43554cdda6c71bd6be35bd8fd98a3910800
eduard.levchenko@arcelormittal.com <eduard.levchenko@arcelormittal.com>
2021-03-26 17:26:48 +0200

    sql

--- sql/Администрирование/wsd/Скрипт тестирования функций окна WSD.sql
+++ sql/Администрирование/wsd/Скрипт тестирования функций окна WSD.sql
@@ -1,6 +1,8 @@
 use [KRR-PA-CNT-Railway]
 declare @id_way int = 225
+declare @status int = 2
 
 
 -- “ест выборки вагонов по на пути детально
 select * from [IDS].[get_view_wagons_of_way](@id_way) order by position
+select * from [IDS].[get_outgoing_sostav_of_status](@status)
