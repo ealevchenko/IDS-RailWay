ae8847d8205a9e2f1e40e7e49b42e6f7deb6f8ab
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-05-19 17:20:32 +0300

    correct text module

--- WEB_UI/Scripts/module/table_wio.js
+++ WEB_UI/Scripts/module/table_wio.js
@@ -12,15 +12,13 @@
         'default':  //default language: ru
         {
             'field_id': 'id строки',
-            'field_station': 'Станция',
-            'field_park_way': 'Парк',
-            'field_way': 'Путь',
-            'field_way_start': 'Поставили на путь',
-            'field_way_end': 'Убрали с пути',
-            'field_outer_way': 'Перегон',
-            'field_outer_way_start': 'Вышел на перегон',
-            'field_outer_way_end': 'Пришел с перегона',
-            'field_position': '№ поз.',
+            'field_operations': 'Операция',
+            'field_operation_start': 'Начало операции',
+            'field_operation_end': 'Окончание операции',
+            'field_condition': 'Разметка',
+            'field_loading_status': 'Сотояние загрузки',
+            'field_locomotive1': 'Сотояние загрузки',
+            'field_locomotive2': 'Сотояние загрузки',
             'field_note': 'Примечание',
             'field_create': 'Строка создана',
             'field_close': 'Строка закрыта',
@@ -30,21 +28,18 @@
             'title_button_buffer': 'Буфер',
             'title_button_excel': 'Excel',
 
-            'mess_load_wim_wagons': 'Загружаю список истории дислокации вагона...',
-
+            'mess_load_wio_wagons': 'Загружаю список истории операций вагона...',
         },
         'en':  //default language: English
         {
             'field_id': 'row id',
-            'field_station': 'Station',
-            'field_park_way': 'Park',
-            'field_way': 'Path',
-            'field_way_start': 'Put on a path',
-            'field_way_end': 'Out of the way',
-            'field_outer_way': 'Ferry',
-            'field_outer_way_start': 'Out on the track',
-            'field_outer_way_end': 'Came from the stretch',
-            'field_position': 'Pos. no.',
+            'field_operations': 'Operation',
+            'field_operation_start': 'Operation start',
+            'field_operation_end': 'End of operation',
+            'field_condition': 'Markup',
+            'field_loading_status': 'Loading status',
+            'field_locomotive1': 'Loading status',
+            'field_locomotive2': 'Loading status',
             'field_note': 'Note',
             'field_create': 'String created',
             'field_close': 'The line is closed',
@@ -54,7 +49,7 @@
             'title_button_buffer': 'Buffer',
             'title_button_excel': 'Excel',
 
-            'mess_load_wim_wagons': 'Loading the list of wagon deployment history ...',
+            'mess_load_wio_wagons': 'Loading the list of wagon operations history ...',
         }
     };
     // Определлим список текста для этого модуля
@@ -65,7 +60,7 @@
     // Перечень полей
     var list_collums = [
         {
-            field: 'wim_button_view',
+            field: 'wio_button_view',
             targets: 0,
             data: null,
             defaultContent: '<button class="btn"><i class="far fa-eye"></i></button>',
@@ -74,7 +69,7 @@
             width: "20px"
         },
         {
-            field: 'wim_id',
+            field: 'wio_id',
             data: function (row, type, val, meta) {
                 return row.id;
             },
@@ -82,84 +77,66 @@
             title: langView('field_id', App.Langs), width: "30px", orderable: true, searchable: true
         },
         {
-            field: 'wim_station',
+            field: 'wio_operations',
             data: function (row, type, val, meta) {
-                var station = row.Directory_Station ? row.Directory_Station : null;
-                return station ? station['station_name_' + App.Lang] : null;
+                var operations = row.Directory_WagonOperations ? row.Directory_WagonOperations : null;
+                return operations ? operations['operation_name_' + App.Lang] : null;
             },
             className: 'dt-body-center',
-            title: langView('field_station', App.Langs), width: "50px", orderable: false, searchable: true
+            title: langView('field_operations', App.Langs), width: "50px", orderable: false, searchable: true
         },
         {
-            field: 'wim_park_way',
+            field: 'wio_operation_start',
             data: function (row, type, val, meta) {
-                var way = row.Directory_Ways ? row.Directory_Ways : null;
-                var park = way ? way.Directory_ParkWays : null;
-                return park ? (park['park_name_' + App.Lang]) : null;
+                return getReplaceTOfDT(row.operation_start);
             },
             className: 'dt-body-center',
-            title: langView('field_park_way', App.Langs), width: "100px", orderable: false, searchable: true
+            title: langView('field_operation_start', App.Langs), width: "100px", orderable: true, searchable: false
         },
         {
-            field: 'wim_way',
+            field: 'wio_operation_end',
             data: function (row, type, val, meta) {
-                var way = row.Directory_Ways ? row.Directory_Ways : null;
-                return way ? (way['way_num_' + App.Lang] + ' - ' + way['way_name_' + App.Lang]) : null;
+                return getReplaceTOfDT(row.operation_end);
             },
             className: 'dt-body-center',
-            title: langView('field_way', App.Langs), width: "100px", orderable: false, searchable: true
+            title: langView('field_operation_end', App.Langs), width: "100px", orderable: true, searchable: false
         },
         {
-            field: 'wim_way_start',
+            field: 'wio_condition',
             data: function (row, type, val, meta) {
-                return getReplaceTOfDT(row.way_start);
+                var condition = row.Directory_ConditionArrival ? row.Directory_ConditionArrival : null;
+                return condition ? condition['condition_name_' + App.Lang] : null;
             },
             className: 'dt-body-center',
-            title: langView('field_way_start', App.Langs), width: "100px", orderable: true, searchable: false
+            title: langView('field_condition', App.Langs), width: "100px", orderable: true, searchable: false
         },
         {
-            field: 'wim_way_end',
+            field: 'wio_loading_status',
             data: function (row, type, val, meta) {
-                return getReplaceTOfDT(row.way_end);
+                var loading_status = row.Directory_WagonLoadingStatus ? row.Directory_WagonLoadingStatus : null;
+                return loading_status ? loading_status['loading_status_' + App.Lang] : null;
             },
             className: 'dt-body-center',
-            title: langView('field_way_end', App.Langs), width: "100px", orderable: true, searchable: false
+            title: langView('field_loading_status', App.Langs), width: "100px", orderable: true, searchable: false
         },
         {
-            field: 'wim_outer_way',
+            field: 'wio_locomotive1',
             data: function (row, type, val, meta) {
-                var way = row.Directory_OuterWays ? row.Directory_OuterWays : null;
-                return way ? (way['name_outer_way_' + App.Lang]) : null;
+                return row.locomotive1;
             },
             className: 'dt-body-center',
-            title: langView('field_outer_way', App.Langs), width: "100px", orderable: false, searchable: true
+            title: langView('field_locomotive1', App.Langs), width: "30px", orderable: true, searchable: true
         },
         {
-            field: 'wim_outer_way_start',
+            field: 'wio_locomotive2',
             data: function (row, type, val, meta) {
-                return getReplaceTOfDT(row.outer_way_start);
+                return row.locomotive1;
             },
             className: 'dt-body-center',
-            title: langView('field_outer_way_start', App.Langs), width: "100px", orderable: true, searchable: false
+            title: langView('field_locomotive2', App.Langs), width: "30px", orderable: true, searchable: true
         },
         {
-            field: 'wim_outer_way_end',
-            data: function (row, type, val, meta) {
-                return getReplaceTOfDT(row.outer_way_end);
-            },
-            className: 'dt-body-center',
-            title: langView('field_outer_way_end', App.Langs), width: "100px", orderable: true, searchable: false
-        },
-        {
-            field: 'wim_position',
-            data: function (row, type, val, meta) {
-                return row.position;
-            },
-            className: 'dt-body-center',
-            title: langView('field_position', App.Langs), width: "30px", orderable: false, searchable: false
-        },
-        {
-            field: 'wim_note',
+            field: 'wio_note',
             data: function (row, type, val, meta) {
                 return row.note;
             },
@@ -167,7 +144,7 @@
             title: langView('field_note', App.Langs), width: "30px", orderable: false, searchable: false
         },
         {
-            field: 'wim_create',
+            field: 'wio_create',
             data: function (row, type, val, meta) {
                 return row.create ? (row.create_user + '<br />[' + getReplaceTOfDT(row.create) + ']') : null;
             },
@@ -175,7 +152,7 @@
             title: langView('field_create', App.Langs), width: "100px", orderable: false, searchable: false
         },
         {
-            field: 'wim_close',
+            field: 'wio_close',
             data: function (row, type, val, meta) {
                 return row.close ? (row.close_user + '<br />[' + getReplaceTOfDT(row.close) + ']') : null;
             },
@@ -183,7 +160,7 @@
             title: langView('field_close', App.Langs), width: "100px", orderable: false, searchable: false
         },
         {
-            field: 'wim_parent_id',
+            field: 'wio_parent_id',
             data: function (row, type, val, meta) {
                 return row.parent_id;
             },
@@ -198,40 +175,38 @@
     // таблица истрия внутренего передвижения вагона
     //-----------------------------------------------------------------------
     // Конструктор модуля таблицы внутреней дислокации вагона
-    function table_wim(selector) {
+    function table_wio(selector) {
         if (!selector) {
             throw new Error('No selector provided');
         }
-        this.$t_wim = $(selector);
-        if (this.$t_wim.length === 0) {
+        this.$t_wio = $(selector);
+        if (this.$t_wio.length === 0) {
             throw new Error('Could not find element with selector: ' + selector);
         }
     }
 
     // инициализация полей таблицы вагоны на начальном пути
-    table_wim.prototype.init_columns = function () {
+    table_wio.prototype.init_columns = function () {
         var collums = [];
         //if (this.b_detali_wir) collums.push('arr_car_details_control');
-        collums.push('wim_button_view');
-        collums.push('wim_id');
-        collums.push('wim_position');
-        collums.push('wim_station');
-        collums.push('wim_park_way');
-        collums.push('wim_way');
-        collums.push('wim_way_start');
-        collums.push('wim_way_end');
-        collums.push('wim_outer_way');
-        collums.push('wim_outer_way_start');
-        collums.push('wim_outer_way_end');
-        collums.push('wim_note');
-        collums.push('wim_create');
-        collums.push('wim_close');
-        collums.push('wim_parent_id');
+        collums.push('wio_button_view');
+        collums.push('wio_id');
+        collums.push('wio_operations');
+        collums.push('wio_operation_start');
+        collums.push('wio_operation_end');
+        collums.push('wio_condition');
+        collums.push('wio_loading_status');
+        collums.push('wio_locomotive1');
+        collums.push('wio_locomotive2');
+        collums.push('wio_note');
+        collums.push('wio_create');
+        collums.push('wio_close');
+        collums.push('wio_parent_id');
         return init_columns(collums, list_collums);
     };
     //
-    table_wim.prototype.init = function () {
-        this.obj_wim = this.$t_wim.DataTable({
+    table_wio.prototype.init = function () {
+        this.obj_wio = this.$t_wio.DataTable({
             "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
             "paging": true,
             "searching": true,
@@ -292,34 +267,34 @@
         });
     };
     // Показать данные 
-    table_wim.prototype.view = function (data) {
-        this.obj_wim.clear();
-        this.obj_wim.rows.add(data);
-        this.obj_wim.order([1, 'desc']);
-        this.obj_wim.draw();
+    table_wio.prototype.view = function (data) {
+        this.obj_wio.clear();
+        this.obj_wio.rows.add(data);
+        this.obj_wio.order([1, 'desc']);
+        this.obj_wio.draw();
     };
     // загрузить данные по id
-    table_wim.prototype.load_of_id = function (id) {
+    table_wio.prototype.load_of_id = function (id) {
         if (id) {
-            LockScreen(langView('mess_load_wim_wagons', App.Langs));
-            ids_rwt.getWagonInternalMovementOfID(id, function (list_wim_wagon) {
-                this.view($(list_wim_wagon));
+            LockScreen(langView('mess_load_wio_wagons', App.Langs));
+            ids_rwt.getWagonInternalOperationOfID(id, function (list_wio_wagon) {
+                this.view($(list_wio_wagon));
                 LockScreenOff();
             }.bind(this));
         };
     };
     // загрузить данные по id
-    table_wim.prototype.load_of_id_wir = function (id) {
+    table_wio.prototype.load_of_id_wir = function (id) {
         if (id) {
-            LockScreen(langView('mess_load_wim_wagons', App.Langs));
-            ids_rwt.getWagonInternalMovementOfWIRID(id, function (list_wim_wagon) {
-                this.view(list_wim_wagon);
+            LockScreen(langView('mess_load_wio_wagons', App.Langs));
+            ids_rwt.getWagonInternalOperationOfWIRID(id, function (list_wio_wagon) {
+                this.view(list_wio_wagon);
                 LockScreenOff();
             }.bind(this));
         };
     };
     //
-    App.table_wim = table_wim;
+    App.table_wio = table_wio;
 
     window.App = App;
 })(window);
