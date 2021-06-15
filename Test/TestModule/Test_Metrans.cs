78bb025097b90de30fabd334e433122401079ef1
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-06-15 15:03:54 +0300

    Development of a new data transfer from Metrans via API  - test

--- Test/TestModule/Test_Metrans.cs
+++ Test/TestModule/Test_Metrans.cs
@@ -44,7 +44,7 @@ namespace Test.TestModule
             //mtt.ArrivalToRailWay = false;
             //mtt.FromPath = @"D:\xlm_new";
             //mtt.DeleteFile = true;
-            int res_transfer = mtt.TransferArrivalAPI("https://inform.umtrans.com.ua", "Arcelor1","12345678-","/api/TrainNaturList/Get");
+            int res_transfer = mtt.TransferArrivalAPI("https://inform.umtrans.com.ua", "Arcelor1","12345678-","/api/TrainNaturList");
         }
 
         public void MTTransfer_WebApiClientMT()
