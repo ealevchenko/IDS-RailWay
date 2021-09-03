2dfdf3dc6354431341595940367a97534b5924b7
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-09-03 16:05:26 +0300

    Тестовая форма для тестирования модуля "Операции отправки вагонов на станции АМКР"

--- WEB_UI/Scripts/view/idsrwt/test.js
+++ WEB_UI/Scripts/view/idsrwt/test.js
@@ -8,132 +8,30 @@
     App.Langs = $.extend(true, App.Langs, getLanguages($.Text_Common, App.Lang), getLanguages($.Text_Table, App.Lang));
     App.User_Name = $('input#username').val();
 
-    //var $num_wagon = $('input#num_wagon').val(getUrlVar('num'));
-
     var alert = new ALERT($('div#main-alert')); // Создадим класс ALERTG
 
-    //var $bt_search_wagon = $('button#bt_search_wagon').on('click', function (event) {
-    //    event.preventDefault();
-    //    search_num($num_wagon.val());
-    //});
+    //var TDPW = App.table_dir_park_ways;
+    //var tdpw = new TDPW('div#park-ways'); // Создадим экземпляр таблицы
 
-    //var ids_rwt = new IDS_RWT(App.Lang);                // Создадим класс IDS_RWT
-    //// Определим экземпляр таблица вагоны прибытие
-    //var TAW = App.table_arrival_wagons;
-    //var taw = new TAW('table#tab_arr'); // Создадим экземпляр таблицы
-    //// Определим экземпляр таблица вагоны прибытие
-    //var TWIR = App.table_wir;
-    //var twir = new TWIR('table#tab_wir'); // Создадим экземпляр таблицы
-    //// Определим экземпляр таблица вагоны прибытие
-    //var TOW = App.table_outgoing_wagon;
-    //var tow = new TOW('table#tab_out'); // Создадим экземпляр таблицы
+    //var TDPARK = App.table_dir_park_station;
+    //var tdpark = new TDPARK('div#park'); // Создадим экземпляр таблицы
 
-    //var active_tab = 0;
+    var wsd = App.ids_wsd;
+    var ids_wsd = new wsd();
 
-    //$('a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
-    //    switch (e.target.id) {
-    //        case 'pills-directory-tab': {
-    //            active_tab = 0; break;
-    //        }
-    //        case 'pills-history-tab': {
-    //            active_tab = 1; break;
-    //        }
-    //        case 'pills-arrival-tab': {
-    //            active_tab = 2;
-    //            view_arrival_wagon();
-    //            break;
-    //        };
-    //        case 'pills-wir-tab': {
-    //            active_tab = 3;
-    //            view_wir_wagon();
-    //            break;
-    //        };
-    //        case 'pills-outgoing-tab': {
-    //            active_tab = 4;
-    //            view_outgoing_wagon();
-    //            break;
-    //        };
-    //    }
-    //    //// newly activated tab
-    //    //e.relatedTarget // previous active tab
-    //})
-    //// Показать вагоны в прибытиии
-    //var view_arrival_wagon = function () {
-    //    taw.load_of_num($num_wagon.val());
-    //};
-    //// Показать вагоны на АМКР
-    //var view_wir_wagon = function () {
-    //    twir.load_of_num($num_wagon.val());
-    //};
-    //// Показать вагоны в отправках
-    //var view_outgoing_wagon = function () {
-    //    tow.load_of_num($num_wagon.val());
-    //};
-    //// Проверка номера вагона
-    //var search_num = function (num) {
-    //    alert.clear_message();
-    //    //clear(); // Очистим экран от старой информации
-    //    LockScreen(langView('mess_delay', App.Langs));
-    //    $bt_search_wagon.prop("disabled", true);
-    //    if (!isNumeric(num) || !is_valid_num_wagon(num)) {
-    //        // Ошибка ввода
-    //        alert.out_error_message('Ошибка, введен неправильный номер :' + num);
-    //        taw.view([]);
-    //        twir.view([]);
-    //        tow.view([]);
-    //        $bt_search_wagon.prop("disabled", false);
-    //        LockScreenOff();
-    //    } else {
-    //        switch (active_tab) {
-    //            case 0: {
-    //                $bt_search_wagon.prop("disabled", false);
-    //                LockScreenOff();
-    //                break;
+    var directory = App.ids_directory;
+    var ids_dir = new directory();
 
-    //            };
-    //            case 1: {
-    //                $bt_search_wagon.prop("disabled", false);
-    //                LockScreenOff();
-    //                break;
-    //            };
-    //            case 2: {
-    //                view_arrival_wagon();
-    //                $bt_search_wagon.prop("disabled", false);
-    //                break;
-    //            };
-    //            case 3: {
-    //                view_wir_wagon();
-    //                $bt_search_wagon.prop("disabled", false);
-    //                break;
-    //            };
-    //            case 4: {
-    //                view_outgoing_wagon();
-    //                break;
-    //            };
-    //        }
-    //    }
-
-    //};
-
-    ////// Определим экземпляр таблица вагоны прибытие
-    ////var Tcbs = App.cblist_station;
-    ////var cb_st = new Tcbs('div#dds-station'); // Создадим экземпляр таблицы
-
-    ////var TTWay = App.ids_tree_way;
-    ////var trWay = new TTWay('div#tree-way'); // Создадим экземпляр таблицы
-
-    //var TDWAY = App.table_dir_way;
-    //var tdways = new TDWAY('div#ways'); // Создадим экземпляр таблицы
-
-    var TDPW = App.table_dir_park_ways;
-    var tdpw = new TDPW('div#park-ways'); // Создадим экземпляр таблицы
-
-    var TDPARK = App.table_dir_park_station;
-    var tdpark = new TDPARK('div#park'); // Создадим экземпляр таблицы
 
 
     var MCF = App.modal_confirm_form;
     var mcf_test = new MCF('confirm-test'); // Создадим экземпляр окно сообщений
+
+
+
+    var OP_SEND = App.operation_send;
+    var oper_send = new OP_SEND('div#operation-send'); // Создадим экземпляр
+
 
     $('button#arrival').on('click', function (e) {
         //trWay.update_station_of_id(8);
@@ -148,71 +46,16 @@
 
     // После загрузки документа
     $(document).ready(function ($) {
-        //mcf_test.init();
-        tdpw.init({
-            alert: alert
-        }, function () {
-            tdpw.load();
-        });
-
-        tdpark.init({
-            alert: alert
-        }, function () {
-            tdpark.load_of_station(8);
-        });
-        //tdways.init({
-        //    alert: alert
-        //}, function () {
-        //    tdways.load_of_station_park(23, 161);
-        //});
-
-
-        //var alert = $('div#test-alert');
-        //var FVAL = App.form_validation;
-        //var form_val = new FVAL('#test'); // Создадим экземпляр таблицы
-        //form_val.init(alert, null, function (data) {
-
-        //});
-
-
-
-
-        ////// Прочтем данные из куков
-        ////var list_station = null;
-        ////var select_station_tree = $.cookie("select_station_tree");
-        ////if (select_station_tree) list_station = $.parseJSON(select_station_tree);
-        ////// Инициализируем компонент выбора станций
-        ////cb_st.init(list_station, function (list_station) {
-        ////    // выбраны станции и команда применить
-        ////    // сохраним сокет
-        ////    $.cookie("select_station_tree", JSON.stringify(list_station), { expires: 365 });
-        ////    // Отразим станции
-        ////    trWay.view(list_station);
-        ////});
-        ////// Инициализируем компонент дерево путей
-        ////trWay.init(function (id_station, id_park, id_way, option) {
-        ////    // Обраблтка выбраного пути
-        ////}, function (name, id) {
-        ////    // Обработка события детально
-        ////});
-        ////trWay.view(list_station);
-
-
-        //trWay.view(list_station, 8, 75, 244);
-
-        //$(".checkbox-menu").on("change", "input[type='checkbox']", function () {
-        //    $(this).closest("li").toggleClass("active", this.checked);
-        //});
-
-        //$(document).on('click', '.allow-focus', function (e) {
-        //    e.stopPropagation();
-        //});
-
+        oper_send.init({
+            alert: alert,
+            ids_dir: ids_dir,
+            ids_wsd: ids_wsd,
+        },
+            function () {
+                // Инициализация закончилась
+            });
         LockScreenOff();
-        //taw.init(true);
-        //twir.init(true);
-        //tow.init(true);
-        //search_num($num_wagon.val());
+
     });
 
 })(jQuery); // End of use strict
