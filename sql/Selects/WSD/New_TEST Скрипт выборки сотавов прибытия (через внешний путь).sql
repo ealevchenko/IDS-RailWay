ca1b9827b45768d04263fc4d7c33e50e900678dc
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-10-06 16:02:38 +0300

    sql

--- sql/Selects/WSD/New_TEST Скрипт выборки сотавов прибытия (через внешний путь).sql
+++ sql/Selects/WSD/New_TEST Скрипт выборки сотавов прибытия (через внешний путь).sql
@@ -4,3 +4,5 @@ select * from [IDS].[get_view_sostav_of_all_outer_ways]()
 
 select * from [IDS].[get_view_wagons_of_all_outer_ways]()
 where outer_way_num_sostav = '50-29092021105300'
+
+select * from [IDS].[get_view_wagons_of_sostav_outer_ways]('50-29092021105300');
