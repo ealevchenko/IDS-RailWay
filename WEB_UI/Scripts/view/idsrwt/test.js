1c5250190622a9963b55a6aa13dab956dd23f7d5
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-10-13 16:00:35 +0300

    Тестовая форма

--- WEB_UI/Scripts/view/idsrwt/test.js
+++ WEB_UI/Scripts/view/idsrwt/test.js
@@ -24,8 +24,8 @@
 
 
 
-    var MCF = App.modal_confirm_form;
-    var mcf_test = new MCF('confirm-test'); // Создадим экземпляр окно сообщений
+    //var MCF = App.modal_confirm_form;
+    //var mcf_test = new MCF('confirm-test'); // Создадим экземпляр окно сообщений
 
 
 
@@ -44,14 +44,17 @@
     //var TSOW = App.table_sostav_outer_way;
     //var t_sow = new TSOW('div#sostav');             // Создадим экземпляр
 
-    var TCOW = App.table_cars_outer_way;
-    var t_cow = new TCOW('div#wagons');             // Создадим экземпляр
+    //var TCOW = App.table_cars_outer_way;
+    //var t_cow = new TCOW('div#wagons');             // Создадим экземпляр
 
-    var VAC = App.view_arrival_cars;
-    var vac = new VAC('div#arrival');             // Создадим экземпляр
+    //var VAC = App.view_arrival_cars;
+    //var vac = new VAC('div#arrival');             // Создадим экземпляр
 
     //var TCOW = App.table_cars_outer_way;
     //var t_cow = new TCOW('div#wagons');             // Создадим экземпляр
+
+    var VHOA = App.view_history_operation_arrival;
+    var vhoa = new VHOA('div#history_operation_arrival');             // Создадим экземпляр
 
     $('button#arrival').on('click', function (e) {
         //var data = oper_send.operation;
@@ -70,7 +73,16 @@
 
     // После загрузки документа
     $(document).ready(function ($) {
-
+        vhoa.init({
+            alert: null,
+            ids_dir: null,
+            ids_wsd: null,
+            fn_db_update: function (list) {
+            }.bind(this),
+        }, function (init) {
+/*            vac.view(123);//880*/
+            LockScreenOff();
+        });
         //t_sow.init({
         //    alert: alert,
         //    type_report: 'arrival-outer-way',  // Прибвыающие составы на внешнем пути
@@ -80,16 +92,16 @@
         //    //t_wagons.load_of_way(218, 54152087);//112
         //    //t_wagons.load_of_way(218);//112
         //});
-        vac.init({
-            alert: null,
-            ids_dir: null,
-            ids_wsd: null,
-            fn_db_update: function (list) {
-            }.bind(this),
-        }, function (init) {
-            vac.view(123);//880
-            //LockScreenOff();
-        });
+        //vac.init({
+        //    alert: null,
+        //    ids_dir: null,
+        //    ids_wsd: null,
+        //    fn_db_update: function (list) {
+        //    }.bind(this),
+        //}, function (init) {
+        //    vac.view(123);//880
+        //    LockScreenOff();
+        //});
         //t_cow.init({
         //    alert: alert,
         //    type_report: 'arrival-wagons-outer-way',  // Прибвыающие вагоны на внешнем пути
