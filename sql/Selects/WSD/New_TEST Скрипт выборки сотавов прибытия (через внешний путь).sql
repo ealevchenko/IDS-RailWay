3f00b63811db0c0e96d2d09065600e8f996f9f80
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-10-20 12:03:47 +0300

    SQL правил функции  + добавлял поля станция фактического прибытия  + добавил функцию показать составы по станции отправки

--- sql/Selects/WSD/New_TEST Скрипт выборки сотавов прибытия (через внешний путь).sql
+++ sql/Selects/WSD/New_TEST Скрипт выборки сотавов прибытия (через внешний путь).sql
@@ -7,10 +7,10 @@ use [KRR-PA-Test-Railway]
 
 --select * from [IDS].[get_view_wagons_of_sostav_outer_ways]('50-29092021105300');
 
-declare @start datetime = Convert(datetime,'2021-09-01 00:00:00',120)
-declare @stop datetime = Convert(datetime,'2021-10-30 23:59:59',120)
+--declare @start datetime = Convert(datetime,'2021-09-01 00:00:00',120)
+--declare @stop datetime = Convert(datetime,'2021-10-30 23:59:59',120)
 
-select * from [IDS].[get_view_sostav_of_period_operation_send](@start,@stop);
---select * from [IDS].[get_view_wagons_of_period_operation_send](@start,@stop);
+--select * from [IDS].[get_view_sostav_of_period_operation_send](@start,@stop);
+----select * from [IDS].[get_view_wagons_of_period_operation_send](@start,@stop);
 
-select * from [IDS].[get_view_wagons_of_sostav_outer_ways]('83-29092021140600');
+select * from [IDS].[get_view_wagons_of_sostav_outer_ways]('51-29092021110900') order by outer_way_position;
