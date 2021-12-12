ae1aab01e08f2de50ff7cf30e0fa5ff5beeeac07
ealevchenko <eduard.levchenko@arcelormittal.com>
2021-12-12 18:41:55 +0200

    Добавил модуль выполнения сдачи вагонов на УЗ

---
+++ WEB_UI/Scripts/module/wsd/view_outgoing_cars.js
@@ -0,0 +1,299 @@
+﻿(function (window) {
+    'use strict';
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+    // Определим язык
+    App.Lang = ($.cookie('lang') === undefined ? 'ru' : $.cookie('lang'));
+
+    // Массив текстовых сообщений 
+    $.Text_View =
+    {
+        'default':  //default language: ru
+        {
+            'vogc_card_header_detali': 'ИНФОРМАЦИЯ ПО СОСТАВУ',
+            'vogc_card_table_cars': 'Предъявленные вагоны',
+            'vogc_card_info_cars': 'Информация по вагону',
+            'vogc_card_list_cars': 'Вагоны',
+
+
+            
+            //'card_header_panel': 'ВЫПОЛНИТЬ ОПЕРАЦИЮ "ОТПРАВИТЬ СОСТАВОВ НА СТАНЦИИ АМКР"',
+            //'card_header_on': 'ОТПРАВИТЬ НА СТАНЦИЮ',
+            //'card_header_from': 'ОТПРАВИТЬ СО СТАНЦИ',
+            //'fieldset_on_table_title': 'Сформированный состав',
+            //'title_label_station_from': 'Станция отправления:',
+            //'title_placeholder_station_from': 'Станция отправления:',
+            //'title_label_way': 'Путь отправления:',
+            //'title_placeholder_way': 'Выберите путь',
+            //'fieldset_on_as_table_title': 'Прибывающие составы на выбранную станцию',
+
+            //'title_label_station_on': 'Станция прибытия:',
+            //'title_placeholder_station_on': 'Станция прибытия:',
+            //'title_label_outer_way': 'Внешний путь:',
+            //'title_placeholder_outer_way': 'Внешний путь',
+            //'title_label_locomotive1': 'Локомотив №1:',
+            //'title_label_locomotive2': 'Локомотив №2:',
+            //'title_placeholder_locomotive': ' № локомотива',
+            //'title_time_aplly': 'Время выполнения',
+            //'title_placeholder_time_aplly': 'Время выполнения',
+
+            //'title_form_apply': 'Выполнить?',
+
+            //'title_button_export': 'Экспорт',
+            //'title_button_buffer': 'Буфер',
+            //'title_button_excel': 'Excel',
+            //'title_button_cancel': 'Отменить',
+            //'title_button_return': 'Вернуть',
+
+            //'title_add_ok': 'ВЫПОЛНИТЬ',
+
+
+            //'mess_error_equal_locomotive': 'Локомотив №1 и №2 равны',
+            //'mess_error_not_locomotive': 'В справочнике ИДС отсутствует локомотив №',
+            //'mess_error_min_time_aplly': 'Дата выполнения операции не может быть меньше текущей даты, мин. отклонение (мин) =',
+            //'mess_error_max_time_aplly': 'Дата выполнения операции не может быть больше текущей даты, мак. отклонение (мин) =',
+            //'mess_error_not_wagons': 'Не выбраны вагоны для отправления (в окне «ОТПРАВИТЬ СО СТАНЦИ», на пути отправки выберите вагоны и сформируйте состав).',
+            //'mess_error_operation_run': 'При выполнении операции «ОТПРАВИТЬ СО СТАНЦИ» произошла ошибка, код ошибки:',
+
+            //'mess_cancel_operation': 'Операция "ОТПРАВИТЬ СОСТАВОВ НА СТАНЦИИ АМКР" – отменена',
+            //'mess_run_operation_send': 'Выполняю операцию отправки состава на станцию АМКР',
+
+            'vogc_mess_not_id_sostav': 'Состав не выбран, неопределен id',
+
+            //'mess_load_operation': 'Загружаю операции...',
+            //'mess_load_wagons': 'Загружаю вагоны на пути...',
+            //'mess_update_operation': 'Обновляю операции...',
+            'vogc_mess_init_panel': 'Выполняю инициализацию модуля ...',
+            'vogc_mess_load_sostav': 'Загружаю информацию по выбранному составу...',
+            //'mess_destroy_operation': 'Закрываю форму...',
+            //'mess_create_sostav': 'Формирую состав, переношу вагоны...',
+            //'mess_clear_sostav': 'Формирую состав, убираю выбранные вагоны...',
+            //'mess_reverse_sostav': 'Формирую состав, реверс вагонов...',
+        },
+        'en':  //default language: English
+        {
+
+        }
+    };
+    // Определлим список текста для этого модуля
+    App.Langs = $.extend(true, App.Langs, getLanguages($.Text_View, App.Lang));
+
+    var wsd = App.ids_wsd;
+    var directory = App.ids_directory;
+    // Модуль инициализаии компонентов формы
+    var MCF = App.modal_confirm_form; // Создать модальную форму "Окно сообщений"
+    var FC = App.form_control;
+    var alert = App.alert_form;
+
+    // создадим основу формы
+    function div_panel(base) {
+        var row = new base.fc_ui.el_row();
+        var col = new base.fc_ui.el_col('xl', 12, 'mb-1 mt-1');
+        var card_panel = new base.fc_ui.el_card('border-secondary mb-1', 'text-center', null, langView('vogc_card_header_detali', App.Langs));
+        var row_panel = new base.fc_ui.el_row();
+        var col_table_cars = new base.fc_ui.el_col('xl', 5, 'mb-1');
+        var col_info_cars = new base.fc_ui.el_col('xl', 6, 'mb-1');
+        var col_list_cars = new base.fc_ui.el_col('xl', 1, 'mb-1');
+
+        var card_table_cars = new base.fc_ui.el_card('border-success', 'text-center', 'p-1', langView('vogc_card_table_cars', App.Langs));
+        var card_info_cars = new base.fc_ui.el_card('border-warning', 'text-center', 'p-1', langView('vogc_card_info_cars', App.Langs));
+        var card_list_cars = new base.fc_ui.el_card('border-primary', 'text-center', 'p-1', langView('vogc_card_list_cars', App.Langs));
+
+        this.$table_cars = card_table_cars.$body;
+        this.$info_cars = card_info_cars.$body;
+        this.$list_cars = card_list_cars.$body;
+
+        col_table_cars.$col.append(card_table_cars.$card);
+        col_info_cars.$col.append(card_info_cars.$card);
+        col_list_cars.$col.append(card_list_cars.$card);
+
+        row_panel.$row.append(col_table_cars.$col).append(col_info_cars.$col).append(col_list_cars.$col);
+
+        card_panel.$body.append(row_panel.$row);
+        this.$element = row.$row.append(col.$col.append(card_panel.$card));
+    };
+
+    function view_outgoing_cars(selector) {
+        if (!selector) {
+            throw new Error('Не указан селектор');
+        }
+        this.$panel = $(selector);
+        if (this.$panel.length === 0) {
+            throw new Error('Не удалось найти элемент с селектором: ' + selector);
+        }
+        this.selector = this.$panel.attr('id');
+        this.fc_ui = new FC();
+    }
+    // Функция обновить данные из базы list-список таблиц, update-обновить принудительно, callback-возврат список обновленных таблиц
+    view_outgoing_cars.prototype.load_db = function (list, update, callback) {
+        if (list) {
+            this.ids_dir.load(list, false, update, function (result) {
+                if (typeof callback === 'function') {
+                    callback(result);
+                }
+            });
+        };
+    };
+    // инициализация модуля
+    view_outgoing_cars.prototype.init = function (options) {
+        this.result_init = true;
+        // теперь выполним инициализацию, определим основные свойства
+
+        // Создать модальную форму "Окно сообщений"
+        this.modal_confirm_form = new MCF(this.selector); // Создадим экземпляр окно сообщений
+        this.modal_confirm_form.init();
+
+        this.settings = $.extend({
+            alert: null,
+            ids_dir: null,
+            ids_wsd: null,
+            fn_db_update: null,
+            fn_init: null,
+        }, options);
+        // Создадим ссылку на модуль работы с базой данных
+        this.ids_dir = this.settings.ids_dir ? this.settings.ids_dir : new directory();
+        this.ids_wsd = this.settings.ids_wsd ? this.settings.ids_wsd : new wsd();
+
+        this.id_sostav = null;
+        this.select_sostav = null;
+        // Сообщение
+        LockScreen(langView('vogc_mess_init_panel', App.Langs));
+        //----------------------------------
+        // Создать макет панели
+        var panelElement = new div_panel(this);
+        this.$panel.empty();
+        this.$table_cars = panelElement.$table_cars;
+        this.$info_cars = panelElement.$info_cars;
+        // Добавим список вагонов
+        //<div class="list-group" id="list-cars-not-outgoing" role="tablist">
+        //</div>
+        var $div_tablist = new this.fc_ui.el_div(null, 'list-group');
+        $div_tablist.$div.attr('role', 'tablist');
+        this.tablist = $div_tablist.$div;
+        panelElement.$list_cars.append(this.tablist);
+        this.$list_cars = panelElement.$list_cars;
+
+
+
+
+
+        //this.alert_on = new alert(panelElement.$alert_on);
+        //this.alert_from = new alert(panelElement.$alert_from);
+
+        this.$panel.append(panelElement.$element);
+
+        // Создадим и добавим макет таблицы
+
+        // Загрузим справочные данные, определим поля формы правки
+        this.load_db(['station', 'ways', 'outer_ways', 'locomotive'], false, function (result) {
+
+        }.bind(this));
+    };
+    // Открыть модуль 
+    view_outgoing_cars.prototype.open = function (id_sostav) {
+        if (id_sostav) {
+            LockScreen(langView('vogc_mess_load_sostav', App.Langs));
+            this.ids_wsd.getOutgoingSostavOfID(id_sostav, function (sostav) {
+                this.select_sostav = sostav;
+                // Показать вагоны которые не сдали.
+                this.view_cars_not_outgoing(this.select_sostav && this.select_sostav.OutgoingCars ? this.select_sostav.OutgoingCars: []);
+                LockScreenOff();
+            }.bind(this));
+        } else {
+            this.out_clear();
+            this.out_warning(langView('vogc_mess_not_id_sostav', App.Langs));
+        }
+
+    };
+
+    view_outgoing_cars.prototype.view_cars_not_outgoing = function (cars) {
+        this.tablist.empty();
+        $.each(cars, function (i, el) {
+            var icon_arrival = (el.parent_wir_id ? 'fa-retweet' : 'fa-train');
+            var link = $('<a class="list-group-item list-group-item-action" id="' + el.id + '" data-toggle="list" href="#" role="tab" aria-controls="">' + el.num + ' <i class="fa ' + icon_arrival + '" aria-hidden="true"></i> ' + (el.vagonnik ? '<i class="fa fa-thumbs-o-up" aria-hidden="true" title="Вагон размечен"></i>' : '') + '</a>');
+            if (el.parent_wir_id) {
+                link.addClass('disabled');
+            }
+            this.tablist.append(link);
+
+        }.bind(this));
+        // Определим событие
+        $('#list-cars-not-outgoing a').on('click', function (e) {
+            e.preventDefault();
+            var id = $(this).attr('id');
+            cars_detali.view_select_car(id, true);
+        });
+    };
+    //--------------------------------------------------------------------------------
+    // Показать
+    view_outgoing_cars.prototype.show = function () {
+        this.$panel.show();
+    }
+    // Скрыть
+    view_outgoing_cars.prototype.hide = function () {
+        this.$panel.hide();
+    }
+    // Очистить сообщения
+    view_outgoing_cars.prototype.out_clear = function () {
+        if (this.settings.alert) {
+            this.settings.alert.clear_message()
+        }
+    }
+    // Показать ошибки
+    view_outgoing_cars.prototype.out_error = function (message) {
+        if (this.settings.alert) {
+            this.settings.alert.out_error_message(message)
+        }
+    }
+    // Показать предупреждения
+    view_outgoing_cars.prototype.out_warning = function (message) {
+        if (this.settings.alert) {
+            this.settings.alert.out_warning_message(message)
+        }
+    }
+    // Показать сообщения о выполнении действий
+    view_outgoing_cars.prototype.out_info = function (message) {
+        if (this.settings.alert) {
+            this.settings.alert.out_info_message(message)
+        }
+    }
+    //--------------------------------------------------------------------------------
+    // Очистить объект
+    view_outgoing_cars.prototype.destroy = function () {
+        LockScreen(langView('mess_destroy_operation', App.Langs));
+        // Очистить модальную форму подтверждения
+        if (this.modal_confirm_form) {
+            this.modal_confirm_form.destroy();
+            this.modal_confirm_form = null;
+        }
+        // Очистить форму выбора пути отправки
+        if (this.form_setup_from) {
+            this.form_setup_from.destroy();
+            this.form_setup_from = null;
+        }
+        // Очистить форму выбора куда отправить
+        if (this.form_setup_on) {
+            this.form_setup_on.destroy();
+            this.form_setup_on = null;
+        }
+
+        // Уберем модуль (Таблица собранный состав для отправки детально)
+        if (this.tab_cars_on) {
+            this.tab_cars_on.destroy();
+            this.tab_cars_on = null;
+        }
+        // Уберем модуль (Таблица вагоны на на пути отправки детально)
+        if (this.tab_cars_from) {
+            this.tab_cars_from.destroy();
+            this.tab_cars_from = null;
+
+        }
+        this.$panel.empty(); // empty in case the columns change
+        LockScreenOff();
+    };
+
+    App.view_outgoing_cars = view_outgoing_cars;
+
+    window.App = App;
+})(window);
