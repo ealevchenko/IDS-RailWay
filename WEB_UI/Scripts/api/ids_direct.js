4689e51d9aaed24d53dc8f26a64258cdb18c9f77
ealevchenko <eduard.levchenko@arcelormittal.com>
2022-03-21 14:45:38 +0200

    Модуль api исправил отключение блокировки экрана

--- WEB_UI/Scripts/api/ids_direct.js
+++ WEB_UI/Scripts/api/ids_direct.js
@@ -49,7 +49,7 @@
         var result = [];
         var out_load = function (process) {
             if (process === 0) {
-                LockScreenOff();
+                if (lock) { LockScreenOff(); }
                 if (typeof callback === 'function') {
                     callback(result);
                 }
