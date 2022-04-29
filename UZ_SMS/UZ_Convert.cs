baf394778f5b2b9792fe13405c565c4f5ed1c4e6
ealevchenko <eduard.levchenko@arcelormittal.com>
2022-04-29 10:29:18 +0300

    автоформат

--- UZ_SMS/UZ_Convert.cs
+++ UZ_SMS/UZ_Convert.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using IDSLogs;
+using IDSLogs.Enum;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-using IDSLogs.Enum;
-using IDSLogs;
 
 namespace UZ
 {
