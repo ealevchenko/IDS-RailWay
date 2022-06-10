0ed2a6d79a49c7e1d71dbb4372168268190210f9
ealevchenko <eduard.levchenko@arcelormittal.com>
2022-06-10 16:50:20 +0300

    sql

---
+++ sql/SQLQuery1.sql
@@ -0,0 +1,13 @@
+/****** Скрипт для команды SelectTopNRows из среды SSMS  ******/
+SELECT TOP (1000) [num_doc]
+      ,[revision]
+      ,[status]
+      ,[code_from]
+      ,[code_on]
+      ,[dt]
+      ,[xml_doc]
+      ,[num_uz]
+      ,[close]
+      ,[close_message]
+  FROM [KRR-PA-CNT-Railway].[IDS].[UZ_DOC]
+where [num_uz] =500512
