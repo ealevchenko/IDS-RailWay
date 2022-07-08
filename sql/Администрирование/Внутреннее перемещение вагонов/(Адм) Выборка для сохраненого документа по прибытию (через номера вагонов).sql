47867392ef0653eea48f7b681fe179a2689f3d70
ealevchenko <eduard.levchenko@arcelormittal.com>
2022-07-08 16:16:06 +0300

    sql

--- sql/Администрирование/Внутреннее перемещение вагонов/(Адм) Выборка для сохраненого документа по прибытию (через номера вагонов).sql
+++ sql/Администрирование/Внутреннее перемещение вагонов/(Адм) Выборка для сохраненого документа по прибытию (через номера вагонов).sql
@@ -1,11 +1,11 @@
 /****** Скрипт для команды SelectTopNRows из среды SSMS  ******/
 SELECT max([id])
   FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon]
-  where num in (67700484,65734428,68609973,66449380,68534965,65302689,60423803,60282324,60226966,64560626)
+  where num in (18922039)
   group by num 
 
 
- select [id_document] FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where id in (SELECT max([id]) FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where num in (67700484,65734428,68609973,66449380,68534965,65302689,60423803,60282324,60226966,64560626) group by num)
+ select [id_document] FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where id in (SELECT max([id]) FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where num in (18922039) group by num)
 
  SELECT TOP (1000) [id]
       ,[id_doc_uz]
@@ -29,4 +29,4 @@ SELECT max([id])
       ,[change_user]
       ,[manual]
   FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Document]
-  where id in (select [id_document] FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where id in (SELECT max([id]) FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where num in (67700484,65734428,68609973,66449380,68534965,65302689,60423803,60282324,60226966,64560626) group by num))
+  where id in (select [id_document] FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where id in (SELECT max([id]) FROM [KRR-PA-CNT-Railway].[IDS].[Arrival_UZ_Vagon] where num in (18922039) group by num))
