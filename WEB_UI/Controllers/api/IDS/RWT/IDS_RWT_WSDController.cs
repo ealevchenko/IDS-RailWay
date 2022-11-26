3dad5ae494492eb70e5a88ff15b65d110ff5c66b
ealevchenko <eduard.levchenko@arcelormittal.com>
2022-11-26 16:51:41 +0200

    api controller добавил GetCurrentOperationWagonOfNum(int num)

--- WEB_UI/Controllers/api/IDS/RWT/IDS_RWT_WSDController.cs
+++ WEB_UI/Controllers/api/IDS/RWT/IDS_RWT_WSDController.cs
@@ -808,6 +808,89 @@ namespace WEB_UI.Controllers.api.IDS.RWT
 
     #endregion
 
+    #region ОТЧЕТЫ Транспортный департамент (Обновленный АРМ)
+    public class current_operation_wagon
+    {
+        public int num { get; set; }
+        public int? id_wagons_rent { get; set; }
+        public int? curr_wagons_rent_id_operator { get; set; }
+        public string curr_wagons_rent_operators_ru { get; set; }
+        public string curr_wagons_rent_operators_en { get; set; }
+        public string curr_wagons_rent_operator_abbr_ru { get; set; }
+        public string curr_wagons_rent_operator_abbr_en { get; set; }
+        public DateTime? curr_wagons_rent_start { get; set; }
+        public DateTime? curr_wagons_rent_end { get; set; }
+        public bool? curr_wagons_rent_operator_paid { get; set; }
+        public string curr_wagons_rent_operator_color { get; set; }
+        public int? curr_wagons_rent_id_limiting { get; set; }
+        public string curr_wagons_rent_limiting_name_ru { get; set; }
+        public string curr_wagons_rent_limiting_name_en { get; set; }
+        public string curr_wagons_rent_limiting_abbr_ru { get; set; }
+        public string curr_wagons_rent_limiting_abbr_en { get; set; }
+        public int wagon_id_countrys { get; set; }
+        public int? wagon_adm { get; set; }
+        public string wagon_adm_name_ru { get; set; }
+        public string wagon_adm_name_en { get; set; }
+        public string wagon_adm_abbr_ru { get; set; }
+        public string wagon_adm_abbr_en { get; set; }
+        public int wagon_id_genus { get; set; }
+        public int? wagon_rod { get; set; }
+        public string wagon_rod_name_ru { get; set; }
+        public string wagon_rod_name_en { get; set; }
+        public string wagon_rod_abbr_ru { get; set; }
+        public string wagon_rod_abbr_en { get; set; }
+        public int wagon_id_owner { get; set; }
+        public string wagon_owner_wagon_ru { get; set; }
+        public string wagon_owner_wagon_en { get; set; }
+        public string wagon_owner_wagon_abbr_ru { get; set; }
+        public string wagon_owner_wagon_abbr_en { get; set; }
+        public double wagon_gruzp { get; set; }
+        public double? wagon_tara { get; set; }
+        public int wagon_kol_os { get; set; }
+        public string wagon_usl_tip { get; set; }
+        public DateTime? wagon_date_rem_uz { get; set; }
+        public DateTime? cwagon_date_rem_vag { get; set; }
+        public int? wagon_id_type_ownership { get; set; }
+        public int? cwagon_sign { get; set; }
+        public string wagon_factory_number { get; set; }
+        public string wagon_inventory_number { get; set; }
+        public int? wagon_year_built { get; set; }
+        public bool? wagon_exit_ban { get; set; }
+        public string wagon_note { get; set; }
+        public int? wagon_sobstv_kis { get; set; }
+        public bool? wagon_bit_warning { get; set; }
+        public DateTime wagon_create { get; set; }
+        public string wagon_create_user { get; set; }
+        public DateTime? wagon_change { get; set; }
+        public string wagon_change_user { get; set; }
+        public bool? wagon_closed_route { get; set; }
+        public string wagon_new_construction { get; set; }
+        public long? wir_id { get; set; }
+        public string wir_note { get; set; }
+        public int? arrival_id_condition { get; set; }
+        public string arrival_condition_name_ru { get; set; }
+        public string arrival_condition_name_en { get; set; }
+        public string arrival_condition_abbr_ru { get; set; }
+        public string arrival_condition_abbr_en { get; set; }
+        public bool? arrival_condition_repairs { get; set; }
+        public int? current_id_condition { get; set; }
+        public string current_condition_name_ru { get; set; }
+        public string currentn_condition_name_en { get; set; }
+        public string current_condition_abbr_ru { get; set; }
+        public string current_condition_abbr_en { get; set; }
+        public bool? current_condition_repairs { get; set; }
+        public DateTime? current_condition_create { get; set; }
+        public string current_condition_create_user { get; set; }
+        public string instructional_letters_num { get; set; }
+        public DateTime? instructional_letters_datetime { get; set; }
+        public int? instructional_letters_station_code { get; set; }
+        public string instructional_letters_station_name { get; set; }
+        public string instructional_letters_note { get; set; }
+        public DateTime? last_date_outgoing { get; set; }
+        public DateTime? last_date_outgoing_act { get; set; }
+    }
+    #endregion
+
     #region СЕРВИС Коммерческое состояние
 
     #endregion
@@ -1759,6 +1842,26 @@ namespace WEB_UI.Controllers.api.IDS.RWT
             {
                 IDS_WIR ids_wir = new IDS_WIR(service.WebAPI_IDS);
                 List<ReportBorderCrossing> result = ids_wir.GetReportBorderCrossingOfNums(nums);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        #endregion
+
+        #region ОТЧЕТЫ Транспортный департамент (Обновленный АРМ)
+        // GET: api/ids/rwt/wsd/report/td/current_operation_wagon/num/65201857
+        [Route("report/td/current_operation_wagon/num/{num:int}")]
+        [ResponseType(typeof(current_operation_wagon))]
+        public IHttpActionResult GetCurrentOperationWagonOfNum(int num)
+        {
+            try
+            {
+                System.Data.SqlClient.SqlParameter p_num = new System.Data.SqlClient.SqlParameter("@num", num);
+                string sql = "select * from [IDS].[get_current_operation_wagon_of_num](@num)";
+                current_operation_wagon result = db.Database.SqlQuery<current_operation_wagon>(sql, p_num).FirstOrDefault();
                 return Ok(result);
             }
             catch (Exception e)
