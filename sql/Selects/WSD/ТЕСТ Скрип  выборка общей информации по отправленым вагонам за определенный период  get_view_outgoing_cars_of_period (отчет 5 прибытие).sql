f86151517423f812c8c90a304770ff063dcb277e
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-02-13 17:05:25 +0200

    sql

--- sql/Selects/WSD/ТЕСТ Скрип  выборка общей информации по отправленым вагонам за определенный период  get_view_outgoing_cars_of_period (отчет 5 прибытие).sql
+++ sql/Selects/WSD/ТЕСТ Скрип  выборка общей информации по отправленым вагонам за определенный период  get_view_outgoing_cars_of_period (отчет 5 прибытие).sql
@@ -1,7 +1,17 @@
 use [KRR-PA-CNT-Railway]
-declare @start datetime = Convert(datetime, '2022-08-20 00:00:00', 120)
-declare @stop datetime = Convert(datetime, '2022-08-20 23:59:59', 120)
+declare @start datetime = Convert(datetime, '2023-02-02 00:00:00', 120)
+declare @stop datetime = Convert(datetime, '2023-02-14 23:59:59', 120)
 
  --select [IDS].[get_old_id_arrival_of_wir_parent_id](544683)
 
-select arrival_uz_vagon_cargo_returns, * from [IDS].[get_view_outgoing_cars_of_period](@start,@stop) --where arrival_uz_vagon_cargo_returns is not null
+select outgoing_sostav_id
+,[num]
+,outgoing_uz_document_code_stn_to
+,outgoing_uz_document_station_to_name_ru
+,outgoing_uz_document_to_code_inlandrailway
+,outgoing_uz_document_to_code_railway
+,outgoing_uz_document_to_inlandrailway_name_ru
+,outgoing_uz_document_country_nazn
+,outgoing_uz_document_to_countrys_name_ru
+from [IDS].[get_view_outgoing_cars_of_period](@start,@stop) 
+where outgoing_uz_document_to_code_inlandrailway = 8
