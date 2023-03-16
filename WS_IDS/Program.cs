748ac445690f29300e797e4aa47248fd703182ae
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-03-16 14:35:19 +0200

    1

--- WS_IDS/Program.cs
+++ WS_IDS/Program.cs
@@ -1,18 +1,30 @@
+using System.Reflection;
 using WS_IDS;
 
 System.Environment.CurrentDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<UpdateRent>();
-    }).ConfigureLogging(logging =>
-    {
-        logging.ClearProviders();
-        logging.AddConsole();
-        logging.AddDebug();
-        logging.AddLog4Net();
-    })
-    .Build();
+try
+{
+    IHost host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
+        {
+            services.AddHostedService<UpdateRent>();
+        }).ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+            logging.AddDebug();
+            //logging.AddLog4Net();
+        })
+        .Build();
 
-await host.RunAsync();
+    await host.RunAsync();
+}
+catch (Exception e)
+{
+    Console.Write(e.Message);
+}
+
+
+
+
