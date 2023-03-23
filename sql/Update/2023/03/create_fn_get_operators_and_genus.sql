026c784e5ca6d39fc0a9e5daddb42b7eaace2830
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-03-23 14:45:33 +0200

    sql добавил функцию выборки оператор-род через карточки вагонов

--- sql/Update/2023/03/create_fn_get_operators_and_genus.sql
+++ sql/Update/2023/03/create_fn_get_operators_and_genus.sql
@@ -1,19 +1,52 @@
-use [KRR-PA-CNT-Railway]
-SELECT --rent.[id]
-      --,rent.[num]
+USE [KRR-PA-CNT-Railway]
+GO
+
+/****** Object:  UserDefinedFunction [IDS].[get_last_wms]    Script Date: 23.03.2023 9:39:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+CREATE FUNCTION [IDS].[get_operators_and_genus] 
+ (
+
+ )
+RETURNS 
+@operators_genus Table (
+	[id_operator] [int] NULL,
+	[id_genus] [int] NULL,
+	[operators_ru] [nvarchar](100) NULL,
+	[operators_en] [nvarchar](100) NULL,
+	[operators_abbr_ru] [nvarchar](20) NULL,
+	[operators_abbr_en] [nvarchar](20) NULL,
+	[rod_uz] [int] NULL,
+	[genus_ru] [nvarchar](50) NULL,
+	[genus_en] [nvarchar](50) NULL,
+	[genus_abbr_ru] [nvarchar](5) NULL,
+	[genus_abbr_en] [nvarchar](5) NULL
+)
+AS
+BEGIN
+	insert @operators_genus
+	SELECT
       rent.[id_operator]
 	  ,dir_wag.[id_genus]
       ,dir_oper.[operators_ru]
       ,dir_oper.[operators_en]
 	  ,dir_oper.[abbr_ru] as operators_abbr_ru
       ,dir_oper.[abbr_en] as operators_abbr_en
-      --,rent.[rent_start]
       ,dir_genus.[rod_uz]
       ,dir_genus.[genus_ru]
       ,dir_genus.[genus_en]
 	  ,dir_genus.[abbr_ru] as genus_abbr_ru
       ,dir_genus.[abbr_en] as genus_abbr_en
-	  --into operation_rent
   FROM  [IDS].[Directory_WagonsRent] as rent
   	--> Прибытие состава
 	Left JOIN [IDS].[Directory_Wagons] as dir_wag ON dir_wag.num = rent.num
@@ -21,8 +54,6 @@ SELECT --rent.[id]
 	Left JOIN [IDS].[Directory_GenusWagons] as dir_genus ON dir_genus.id = dir_wag.[id_genus]
   where rent.[rent_end] is null and rent.[id_operator] is not null
   group by 
-  --rent.[id]
-  --, rent.[num]
    dir_wag.[id_genus]
    	  ,dir_genus.[abbr_ru]
       ,dir_genus.[genus_ru]
@@ -34,5 +65,13 @@ SELECT --rent.[id]
       ,dir_oper.[operators_ru]
       ,dir_oper.[abbr_en]
       ,dir_oper.[operators_en]
-  --, rent.[rent_start]
   order by rent.[id_operator], dir_wag.[id_genus]
+  RETURN
+ END
+
+
+
+
+GO
+
+
