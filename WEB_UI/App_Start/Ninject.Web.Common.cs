4c5ecd34675f0fbcc658086fd5cb379c249dc324
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-03-27 12:21:33 +0300

    Добавил контроллер IDS_Directory_CurrencyController

--- WEB_UI/App_Start/Ninject.Web.Common.cs
+++ WEB_UI/App_Start/Ninject.Web.Common.cs
@@ -156,6 +156,9 @@ namespace WEB_UI.App_Start
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.Directory_Ways>>().To<EFIDS.Concrete.EFDirectory_Ways>();
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.Directory_OuterWays>>().To<EFIDS.Concrete.EFDirectory_OuterWays>();
 
+            kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.Directory_Currency>>().To<EFIDS.Concrete.EFDirectory_Currency>();
+
+
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.CardsWagons>>().To<EFIDS.Concrete.EFCardsWagons>();
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.CardsWagonsRepairs>>().To<EFIDS.Concrete.EFCardsWagonsRepairs>();
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.ParksWagons>>().To<EFIDS.Concrete.EFParksWagons>();
@@ -167,6 +170,8 @@ namespace WEB_UI.App_Start
             kernel.Bind<EFUZ.Abstract.IRepository<EFUZ.Entities.Directory_Countrys>>().To<EFUZ.Concrete.EFDirectory_Countrys>();
             kernel.Bind<EFUZ.Abstract.IRepository<EFUZ.Entities.Directory_Stations>>().To<EFUZ.Concrete.EFDirectory_Stations>();
             kernel.Bind<EFUZ.Abstract.IRepository<EFUZ.Entities.Directory_Cargo>>().To<EFUZ.Concrete.EFDirectory_Cargo>();
+
+
 
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.Directory_Reason_Discrepancy>>().To<EFIDS.Concrete.EFDirectory_Reason_Discrepancy>();
             kernel.Bind<EFIDS.Abstract.IRepository<EFIDS.Entities.Directory_DetentionReturn>>().To<EFIDS.Concrete.EFDirectory_DetentionReturn>();
