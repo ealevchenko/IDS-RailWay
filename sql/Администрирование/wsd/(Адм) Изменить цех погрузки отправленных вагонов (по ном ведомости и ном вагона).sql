f0e89cfd567bfca5a703ba6019d4e1eaa3b3b0ff
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-05-08 09:14:23 +0300

    sql функции для выборки периодов ставок

--- sql/Администрирование/wsd/(Адм) Изменить цех погрузки отправленных вагонов (по ном ведомости и ном вагона).sql
+++ sql/Администрирование/wsd/(Адм) Изменить цех погрузки отправленных вагонов (по ном ведомости и ном вагона).sql
@@ -1,11 +1,11 @@
-declare @num_doc int =  162
+declare @num_doc int =  141
 
 SELECT id,[id_division] 
   FROM [KRR-PA-CNT-Railway].[IDS].[Outgoing_UZ_Vagon]
-  where id in (select out_car.id_outgoing_uz_vagon FROM [IDS].[OutgoingSostav] as out_sost Left JOIN [IDS].[OutgoingCars] as out_car ON out_sost.id = out_car.id_outgoing where out_sost.[num_doc]=@num_doc  and  out_car.num in (56256142,61241261,65569238))
+  where id in (select out_car.id_outgoing_uz_vagon FROM [IDS].[OutgoingSostav] as out_sost Left JOIN [IDS].[OutgoingCars] as out_car ON out_sost.id = out_car.id_outgoing where out_sost.[num_doc]=@num_doc  and  out_car.num in (63375968,63530463,63376446,63532659,56195522,63663082,63531545,63663520,63664114,63530083,63664148,63530901,63531370,63663421))
 
---  UPDATE [IDS].[Outgoing_UZ_Vagon]
---   SET [id_division] = 36
+ -- UPDATE [IDS].[Outgoing_UZ_Vagon]
+ --  SET [id_division] = 36
 
--- WHERE id in (548537,548538,548539)
---GO
+ --WHERE id in (545333,545334,545335,545336,545337,545338,545339,545340,545341,545342,545343,545344,545345,545346)
+GO
