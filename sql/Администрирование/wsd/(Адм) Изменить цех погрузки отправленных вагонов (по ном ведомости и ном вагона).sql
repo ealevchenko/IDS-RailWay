cd655bd0a7d51fe955d9e8aca03662681af36167
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-11-29 16:46:51 +0200

    sql адм

--- sql/Администрирование/wsd/(Адм) Изменить цех погрузки отправленных вагонов (по ном ведомости и ном вагона).sql
+++ sql/Администрирование/wsd/(Адм) Изменить цех погрузки отправленных вагонов (по ном ведомости и ном вагона).sql
@@ -1,17 +1,18 @@
-use [KRR-PA-CNT-Railway]
-declare @num_doc int =  1018 
+declare @num_doc int =  444  
 
 SELECT id,[id_division] 
   FROM [KRR-PA-CNT-Railway].[IDS].[Outgoing_UZ_Vagon]
   where id in (select out_car.id_outgoing_uz_vagon 
   FROM [IDS].[OutgoingSostav] as out_sost Left JOIN [IDS].[OutgoingCars] as out_car ON out_sost.id = out_car.id_outgoing 
-  where out_sost.[num_doc]=@num_doc  and  out_car.num in (64738016  ))
+  where out_sost.[num_doc]=@num_doc  
+  --where out_sost.id = 231989
+  and  out_car.num in (58647587,53984084,50612209,51241586,51553360,51418424,50944511 ))
+  
+ -- UPDATE [IDS].[Outgoing_UZ_Vagon]
+ --  SET [id_division] = 8
 
- -- UPDATE [KRR-PA-CNT-Railway].[IDS].[Outgoing_UZ_Vagon]
- --  SET [id_division] = 99
-
- --WHERE id in (604892)
-GO
+ --WHERE id in (603384,603385,603386,603387,603388,603389,603390)
+--GO
 
 SELECT TOP (1000) [id]
       ,[position]
